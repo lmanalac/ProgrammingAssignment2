7906c6520952fcadb51d047fd167dcd5380cbed4
lmanalac <lmanalac@mvesystems.com>
2015-02-21 21:26:36 -0800

    More Pseudo Code

--- cachematrix.R
+++ cachematrix.R
@@ -18,4 +18,17 @@ cacheSolve <- function(x, ...) {
         ## This function is will return the Inverse of Matrix X
         ## However, before compute the Inverse of X, check if it has already been Cached, and if so, return the Cached Copy
   
-}
+        # m <- x$getinverse()
+        # m is inverse matrix of x
+        # if m has been computed before return m not null
+        # if (!is.null(m)) {
+        #     message("getting cahced data")
+        #     return(m)
+        # }
+        # otherwise, compute inverse of x using solve
+        # data<-x$get()
+        # m <- solve(data,....)
+        # x$setinverse(m)
+        # m
+  
+}      
