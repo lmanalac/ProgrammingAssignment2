64f8e89f19aec3532a468a502c92e48f2effc07d
lmanalac <lmanalac@mvesystems.com>
2015-02-21 21:12:58 -0800

    Writing Pseudo Code for CacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
 # Version created by Luis Manalac
+# Step 1 -- Do the assignment in Pseudo-Code
 
 ## Put comments here that give an overall description of what your
 ## functions do
@@ -14,4 +15,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## This function is will return the Inverse of Matrix X
+        ## However, before compute the Inverse of X, check if it has already been Cached, and if so, return the Cached Copy
+  
 }
