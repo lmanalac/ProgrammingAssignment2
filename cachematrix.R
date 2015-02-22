7ed08eed98f8ba55f2aaecc37aff67eb70d98e3d
lmanalac <lmanalac@mvesystems.com>
2015-02-21 21:49:33 -0800

    Can now compute Inverse of A Square Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -18,6 +18,12 @@ cacheSolve <- function(x, ...) {
         ## This function is will return the Inverse of Matrix X
         ## However, before compute the Inverse of X, check if it has already been Cached, and if so, return the Cached Copy
   
+        print("cacheSolve():This is the Matrix I received")
+        print(x)
+  
+        print("cacheSolve():Computing Inverse of the Matrix")
+        y<-solve(x)
+        print(y)
         # m <- x$getinverse()
         # m is inverse matrix of x
         # if m has been computed before return m not null
@@ -30,5 +36,16 @@ cacheSolve <- function(x, ...) {
         # m <- solve(data,....)
         # x$setinverse(m)
         # m
+        
   
 }      
+
+testmatrix<-function()
+{
+  print("testmatrix()")
+  print("testmatrix():This is my Test Matrix")
+  x = matrix(c(1,2,3,4),2,2)
+  print(x)
+  print("Calling cacheSolve")
+  cacheSolve(x)
+}
