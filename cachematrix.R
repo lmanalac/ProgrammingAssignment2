57d5aa8dddb0d63b90dbcd85c5e45f0757093eea
lmanalac <lmanalac@mvesystems.com>
2015-02-21 23:18:03 -0800

    Assignment - Done (Type "testmatrix()" to see demo)
    1. Take an Square Matrix. 2. Create a Global that has the Matrix and its
    Inverse as Members. 3. Initially assign NULL to Inverse. 4. Compute the
    Inverse and Assign to Global. 5. Subsequent calls does not need to
    Compute Inverse, just Get Value from Member Function

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,35 @@
 # Version created by Luis Manalac
+# Type "testmatrix()" to see demo
+
 # Step 1 -- Do the assignment in Pseudo-Code
 
-## Put comments here that give an overall description of what your
+## makeCacheMatrix() is passed a Square Matrix, which it creates a "Global"
+## variable for with a value for the Inverse of the Square Matrix.
+## Initially the Inverse is Set to Null, but can be set to a Value using a 
+## Member function.
+## Once the Inverse has been set to non-Null, the Inverse does not need to be 
+## computed again.
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  print("makeCacheMatrix():This is the Matrix I received")
+  print(x) 
+  
+  m<-NULL
+  
+  set <-function(y) {
+    x<<-y
+    m<<-NULL
+  }
+  get <-function() x
+  setinverse <-function(inverse) m <<-inverse
+  getinverse <-function() m
+  list (set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  
+  
 }
 
 
@@ -19,33 +41,52 @@ cacheSolve <- function(x, ...) {
         ## However, before compute the Inverse of X, check if it has already been Cached, and if so, return the Cached Copy
   
         print("cacheSolve():This is the Matrix I received")
-        print(x)
+        print(x$get())
   
-        print("cacheSolve():Computing Inverse of the Matrix")
-        y<-solve(x)
-        print(y)
-        # m <- x$getinverse()
+        #print("cacheSolve():Computing Inverse of the Matrix")
+        #y<-solve(x)
+        #print(y)
+        m <- x$getinverse()
         # m is inverse matrix of x
         # if m has been computed before return m not null
-        # if (!is.null(m)) {
-        #     message("getting cahced data")
-        #     return(m)
-        # }
+        if (!is.null(m)) {
+             message("cacheSolve():getting cached data")
+             return(m)
+        }
+        message("cacheSolve():computing inverse")
         # otherwise, compute inverse of x using solve
-        # data<-x$get()
-        # m <- solve(data,....)
-        # x$setinverse(m)
-        # m
+        data<-x$get()
+        m <- solve(data)
+        x$setinverse(m)
+        m
         
   
 }      
 
 testmatrix<-function()
-{
+{ 
+  print("testmatrix():1. Start w/ a Square Matrix")
   print("testmatrix()")
   print("testmatrix():This is my Test Matrix")
-  x = matrix(c(1,2,3,4),2,2)
+  x<-matrix(c(1,2,3,4),2,2)
   print(x)
-  print("Calling cacheSolve")
-  cacheSolve(x)
+  
+  print("testmatrix():2. Create a Global Variable that contains both the Matrix and its Inverse.")
+  print("   Initially the Inverse will be NULL")
+  print("testmatrix():Calling makeCacheMatrix()")
+  x_global<-makeCacheMatrix(x)
+  print("testmatrix():Set Value of x_global$get() to Square Matrix")
+  x_global$set(x)
+  print(x_global$get())
+  
+  
+  print("Calling cacheSolve()")
+  ##cacheSolve(x)
+  print("testmatrix():3. Call cacheSolve() for the first time ")
+  print(cacheSolve(x_global))
+  
+  print("testmatrix():4. Call cacheSolve() for the second time ")
+  print(cacheSolve(x_global))
+  
 }
+
